e7e7071ef3e0d7795b2cde3414baee959f521d80
tisonpatrik <patriktison@gmail.com>
2025-11-02 12:13:59 +0100

    preflight

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+sudo pacman -Syu --noconfirm --needed git
+
+# Exit immediately if a command exits with a non-zero status
+set -eEo pipefail
+
+# Define locations
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export INSTALL_PATH="${SCRIPT_DIR}"
+
+
+# Install
+source "$INSTALL_PATH/preflight/all.sh"
+# source "$INSTALL_PATH/packaging/all.sh"
+# source "$INSTALL_PATH/config/all.sh"
+# source "$INSTALL_PATH/login/all.sh"
+# source "$INSTALL_PATH/post-install/all.sh"
---
+++ install/preflight/all.sh
@@ -0,0 +1,2 @@
+source $INSTALL_PATH/preflight/guard.sh
+run_logged $INSTALL_PATH/preflight/pacman.sh
