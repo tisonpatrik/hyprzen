857b43bf011e03a091035fe1169ebfe895932ce2
tisonpatrik <patriktison@gmail.com>
2025-11-04 01:17:02 +0100

    steam suspended

--- install/packaging/all.sh
+++ install/packaging/all.sh
@@ -1,4 +1,4 @@
 bash $INSTALL_PATH/packaging/system.sh
 bash $INSTALL_PATH/packaging/hyprland.sh
 bash $INSTALL_PATH/packaging/base.sh
-bash $INSTALL_PATH/packaging/steam.sh
+# bash $INSTALL_PATH/packaging/steam.sh
