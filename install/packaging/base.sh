5bcea355a30e6effd6f8d18872e2f6189448d784
tisonpatrik <patriktison@gmail.com>
2025-11-08 12:33:17 +0100

    mirror optimalization

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,3 +1,32 @@
-# Install all base packages
-mapfile -t packages < <(grep -v '^#' "$INSTALL_PATH/base.packages" | grep -v '^$')
-sudo pacman -S --noconfirm --needed "${packages[@]}"
+install_base_packages() {
+  mapfile -t packages < <(grep -v '^#' "$INSTALL_PATH/base.packages" | grep -v '^$')
+  sudo pacman -Syu --noconfirm
+  sudo pacman -S --noconfirm --needed "${packages[@]}"
+}
+
+setup_system_trust() {
+  sudo timedatectl set-ntp true
+  sudo update-ca-trust
+  sudo pacman-key --init
+  sudo pacman-key --populate archlinux
+}
+
+configure_mirrorlist() {
+  sudo systemctl enable --now reflector.timer
+  sudo reflector \
+    --protocol https \
+    --latest 20 \
+    --sort rate \
+    --download-timeout 20 \
+    --save /etc/pacman.d/mirrorlist
+}
+
+enable_package_cache() {
+  sudo systemctl enable --now paccache.timer
+}
+
+install_base_packages
+setup_system_trust
+configure_mirrorlist
+sudo pacman -Syyu --noconfirm
+enable_package_cache
