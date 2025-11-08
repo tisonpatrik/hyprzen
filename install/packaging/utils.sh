e1ee4be9db07d27e307607c36dd85e66237c7618
tisonpatrik <patriktison@gmail.com>
2025-11-08 14:06:56 +0100

    cpu opti removed for now

--- install/packaging/utils.sh
+++ install/packaging/utils.sh
@@ -2,8 +2,3 @@
 mapfile -t packages < <(grep -v '^#' "$INSTALL_PATH/utils.packages" | grep -v '^$')
 sudo yay -S --needed --noconfirm "${packages[@]}"
 
-auto_cpufreq_install() {
-  yay -S --needed --noconfirm auto-cpufreq
-  sudo systemctl enable --now auto-cpufreq
-}
-auto_cpufreq_install
