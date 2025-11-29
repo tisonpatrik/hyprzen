42924da9b57a47bfeb153c3653fa8bcf8b4b2419
tisonpatrik <patriktison@gmail.com>
2025-11-29 12:04:12 +0100

    plymouth

--- install/login/plymouth.sh
+++ install/login/plymouth.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
-# Clone catppuccin plymouth theme repository
-TEMP_DIR=$(mktemp -d)
-trap "rm -rf $TEMP_DIR" EXIT
+# Install plymouth and catppuccin theme from AUR
+yay -S --needed --noconfirm plymouth-git plymouth-theme-catppuccin-mocha-git
 
-git clone https://github.com/catppuccin/plymouth.git "$TEMP_DIR/plymouth"
+# Configure plymouth for LUKS encryption
+if [ -f /etc/mkinitcpio.conf ]; then
+  # Add plymouth hook if not already present
+  if ! grep -q "plymouth" /etc/mkinitcpio.conf; then
+    # Add plymouth hook JUST BEFORE encrypt
+    if grep -q "encrypt" /etc/mkinitcpio.conf; then
+      sudo sed -i 's/\(HOOKS=(.*\)encrypt/\1plymouth encrypt/' /etc/mkinitcpio.conf
+    else
+      # If encrypt not found, add before filesystems
+      if grep -q "filesystems" /etc/mkinitcpio.conf; then
+        sudo sed -i 's/\(HOOKS=(.*\)filesystems/\1plymouth filesystems/' /etc/mkinitcpio.conf
+      else
+        # Add at the end if neither found
+        sudo sed -i 's/^HOOKS=(\(.*\))/HOOKS=(\1 plymouth)/' /etc/mkinitcpio.conf
+      fi
+    fi
+  fi
+fi
 
-# Copy mocha theme to plymouth themes directory
-sudo cp -r "$TEMP_DIR/plymouth/themes/catppuccin-mocha" /usr/share/plymouth/themes/
+# Add splash parameter to GRUB (after quiet)
+if [ -f /etc/default/grub ]; then
+  if grep -q "^GRUB_CMDLINE_LINUX_DEFAULT=" /etc/default/grub; then
+    # Add splash after quiet if not already present
+    if ! grep -q "splash" /etc/default/grub; then
+      sudo sed -i 's|\(GRUB_CMDLINE_LINUX_DEFAULT=".*quiet\)"|\1 splash"|' /etc/default/grub
+    fi
+  else
+    echo 'GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"' | sudo tee -a /etc/default/grub > /dev/null
+  fi
+  sudo grub-mkconfig -o /boot/grub/grub.cfg
+fi
 
 # Set plymouth theme
 sudo plymouth-set-default-theme -R catppuccin-mocha
