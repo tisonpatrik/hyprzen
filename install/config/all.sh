8888e838d3147070e159ab0f23258af0439af6a0
tisonpatrik <patriktison@gmail.com>
2025-11-29 20:15:05 +0100

    some configs

--- install/config/all.sh
+++ install/config/all.sh
@@ -8,4 +8,6 @@ cd "$REPO_ROOT" && make apply
 source "$SCRIPT_DIR/docker.sh"
 source "$SCRIPT_DIR/fast-shutdown.sh"
 source "$SCRIPT_DIR/localdb.sh"
+source "$SCRIPT_DIR/mise-work.sh"
+source "$SCRIPT_DIR/network.sh"
 source "$SCRIPT_DIR/zsh.sh"
