a7cb1731446ae9296fc77d2bbb6a2e4c8ef2ec1f
tisonpatrik <patriktison@gmail.com>
2025-11-29 20:08:46 +0100

    some configs

--- install/config/all.sh
+++ install/config/all.sh
@@ -7,4 +7,5 @@ REPO_ROOT="$(cd "$SCRIPT_DIR/../.." && pwd)"
 cd "$REPO_ROOT" && make apply
 source "$SCRIPT_DIR/docker.sh"
 source "$SCRIPT_DIR/fast-shutdown.sh"
+source "$SCRIPT_DIR/localdb.sh"
 source "$SCRIPT_DIR/zsh.sh"
